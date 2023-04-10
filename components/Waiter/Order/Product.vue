eb802e0ae0ec2d67a9b43a59a46c017022db9e30
José Puma <josepuma@sayrin.cl>
2023-04-09 23:35:41 -0500

    Update Product.vue

--- components/Waiter/Order/Product.vue
+++ components/Waiter/Order/Product.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 .relative.border.border-gray-100.bg-white.p-6.rounded-xl
-    p.font-medium.text-sm.text-slate-800 Chancho al palo 
-    p.mt-1.text-sm.text-indigo-500.font-medium $ 25.00
-    .flex.items-center.border.border-slate-200.rounded-xl
+    p.font-medium.text-sm.text-slate-800 Caldo de gallina
+    p.text-sm.text-indigo-500.font-medium $ 25.00
+    .mt-3.flex.items-center.border.border-slate-200.rounded-xl.w-36
         button.w-10.h-10.text-slate-600 &minus;
         input.w-16.h-10.border-transparent.text-center(type="number", value="1")
         button.w-10.h-10.text-slate-600 &plus;
